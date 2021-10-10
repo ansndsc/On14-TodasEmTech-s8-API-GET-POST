fb0eeadf4282b3b0ebfacf312adf64fbd79ea9c9
Ananda Souto <88149291+ansndsc@users.noreply.github.com>
2021-10-10 02:33:00 -0300

    Atualiza exercício feito em aula

--- get-post-20211010T031308Z-001/get-post/testando/data/server.js
+++ get-post-20211010T031308Z-001/get-post/testando/data/server.js
@@ -1,12 +1,15 @@
-//PAREI EM 43MIN SABADO 2 PARTE
+//PAREI EM 2h21min SABADO 2 PARTE
 
 const filmesJson = require("./ghibli.json");//dentro do require coloco o caminho pro meu dado.
 const express = require("express");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json()); //Está fazendo o Body Parse
+
 /*
-1- Quero poder consultar yma lista dos filmes da ghibli por titulo, id e tambem a lista completa.
+1- Quero poder consultar uma lista dos filmes da ghibli por titulo, id e tambem a lista completa.
 2 - se o usuario digitar errado o nome do filme quero retorno do erro.
 3 - quero poder cadastrar um novo filme.
 */
@@ -31,8 +34,18 @@ app.get("/filmes/:id"/*Esses dois pontos significa que agr o que for enviado dep
 
     let filmeEncontrado/*coloquei essa busca dentro da variável "filmeEncontrado"*/ = filmesJson.find(filme => filme.id == idRequest);//encontra a primeira coisa que se identifica com o que eu to pesquisando e retorna. Lógica da estrutura vai ser: "encontre um filme cujo o id é igual ao id enviado na request.(filme => filme.id == id enviado na request)"
 
-    response.status(200).send(filmeEncontrado/*Passei a variável da busca. Posso colocar a estrutura da busca direto aqui dentro ouc oloca-la emum variável, como fiz aqui.*/);
+    response.status(200).send(filmeEncontrado/*Passei a variável da busca. Posso colocar a estrutura da busca direto aqui dentro ou coloca-la em uma variável, como fiz aqui.*/);
 
+})
+
+app.post("/filmes/criar", (request, response) => {
+    let tituloRequest = request.body.title;
+    let descricaoRequest = request.body.description;
+
+    let novofilme = {
+        title: tituloRequest,
+        description: descricaoRequest
+    }; 
 })
 
 app.listen(8081, () => {
@@ -40,6 +53,19 @@ app.listen(8081, () => {
 })
 
 /*
+
 EXPLICAÇÕES
 filmesJson.find(filme => filme.id == idRequest) De onde vem esse "filme" na arrow function? R. > Por baixo do find, assim como por  baixo do filter e afins, ta rolando um for. O for percorre um por um de uma lista. A POSIÇÃO das declarações é a chave, assim como no caso da "(request, response). O find está percorrendo uma array. Na primeira posição do find é o apelido que eu vou dar para o item da minha array. Pode ser qualquer nome. Estou apelidando cada item do objeto e  solicitando o seguinte: Compare o id de cada item com o id que foi mandado na request."
+
+BODY PARSE: 
+Parsear > A ação de uma análise de uma cadeia de string, vai separar, dizer o que é o que para poder manipular.
+
+
+*/
+
+/*
+
+Dúvidas:
+O body do post tem a ver com o body do HTML?
+
 */
